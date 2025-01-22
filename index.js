533f88a324e3feccc1545228e9a9609a02d1ac77
Shaun Newman <shaun.newman.p@gmail.com>
2025-01-22 13:13:38 -0500

    function added and loop adds to object

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(2345);
+// Prompt the user for flavors that are comma seperated
+const flavorPrompt = prompt(`Please enter your flavors seperated by a comma`);
+// split the string on the commas
+const flavorArray = flavorPrompt.split(',');
+
+const flavorFunction = (array) => {
+  // create an empty object
+  const customerOrder = {};
+  // loop though the array
+  for (let i = 0; i < flavorArray.length; i++) {
+    // if key is undefined
+    if (!customerOrder[flavorArray[i]]) {
+      customerOrder[flavorArray[i]] = 1; // key: 1
+    } else {
+      customerOrder[flavorArray[i]]++; // key: [value + 1]
+    }
+  }
+  console.log(customerOrder);
+}
+
+flavorFunction(flavorArray);
